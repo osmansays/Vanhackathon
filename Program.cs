0d8201b296c923f7c6c4e004cb63046b161be239
osman elamin <osmansays@gmail.com>
2019-11-17 02:56:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ImmigrationFormFiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DirectoryInfo directoryInfo = new DirectoryInfo();
+             List<Candidate> candidates= Utility.GetCandidatesFromCSV(@"..\..\..\dataset\HiredCandidates.csv");
+            //   var FormDocument = PdfDocument.FromFile(@"..\..\..\Docs\ESDC-EMP5624.pdf");
+
+            Utility.GenerateCandidatePDF(candidates, @"..\..\..\Docs\ESDC-EMP5624.pdf", @"..\..\..\Docs\ESDC-EMP5624.pdf");
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,54 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ImmigrationFormFiller
+{
+    public static class Utility
+    {
+        public static List<Candidate> GetCandidatesFromCSV(string csvPath)
+        {
+            List<Candidate> candidates = new List<Candidate>();
+            using (var reader = new StreamReader(csvPath))
+            using (var csv = new CsvReader(reader))
+            {
+                csv.Read();
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                    int userid = csv.GetField<int>("UserId");
+                    int jobid = csv.GetField<int>("HiredForJobId");
+                    string skills = csv.GetField("Skills");
+                    string position = csv.GetField("UsersPosition");
+                    Candidate candidate = new Candidate(userid, jobid, skills, position);
+                    candidates.Add(candidate);
+                }
+            }
+            return candidates;
+        }
+
+    
+
+        public static void GenerateCandidatePDF(List<Candidate> candidates,string pdfSourcePath , string pdfDestPath)
+        {
+
+
+            foreach (Candidate _candidate in candidates)
+            {
+                Aspose.Pdf.Facades.Form pdfForm = new Aspose.Pdf.Facades.Form();
+                pdfForm.BindPdf(pdfSourcePath);
+                pdfForm.FillField("EMP5624_E[0].Page2[0].txtF_Emp_Legal[0]", _candidate.UserId);
+                pdfForm.FillField("EMP5624_E[0].Page2[0].txtF_Emp_Legal[0]", _candidate.SkillsAsText);
+               // pdfForm.FillField("EMP5624_E[0].Page2[0].txtF_Emp_Legal[0]", _candidate.UserId);
+              ////  pdfForm.FillField("EMP5624_E[0].Page2[0].txtF_Emp_Legal[0]", _candidate.UserId);
+                pdfForm.Save($"{pdfDestPath.Replace(".pdf","")}{_candidate.UserId}.pdf");
+            }
+
+        }
+
+    }
+}
